f8ef8a07b3e46aa9fefb7db0b21df1bbd3fd91d7
foxhsx <40012964+foxhsx@users.noreply.github.com>
2020-12-30 19:01:18 +0800

    mock

--- docs/frontend/Engineering/sidebar.js
+++ docs/frontend/Engineering/sidebar.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-29 11:19:23
- * @LastEditTime: 2020-12-29 11:27:06
+ * @LastEditTime: 2020-12-30 14:12:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \docs\frontend\Engineering\sidebar.js
@@ -13,6 +13,7 @@ module.exports = [
     path: '/frontend/Engineering/',
     children: [
       '/frontend/Engineering/notes/base',
+      '/frontend/Engineering/notes/mock',
     ]
   }
 ]
