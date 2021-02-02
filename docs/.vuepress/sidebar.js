9dde6f71c9813429d1502a285af19381cb713556
hsx <xahesx111@outlook.com>
2021-02-03 00:15:39 +0800

    vnode

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -4,6 +4,7 @@ module.exports = {
   '/frontend/javascript/': require('../frontend/javascript/sidebar'),
   '/frontend/webpack/': require('../frontend/webpack/sidebar'),
   '/frontend/Engineering/': require('../frontend/Engineering/sidebar'),
+  '/frontend/vue/': require('../frontend/vue/sidebar'),
   '/baodian/zero/': require('../baodian/zero/sidebar'),
   '/baodian/high/': require('../baodian/high/sidebar'),
   '/questionBank/': require('../questionBank/sidebar'),
