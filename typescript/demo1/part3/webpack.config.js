d912dbff06b163592b2195a5a48e0f4a70ee7c55
foxhsx <932163475@qq.com>
2022-03-19 13:58:42 +0800

    安装 babel 和 core

--- typescript/demo1/part3/webpack.config.js
+++ typescript/demo1/part3/webpack.config.js
@@ -6,13 +6,32 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    environment: {
+      arrowFunction: false, // 设置 webpack 打包时不使用剪头函数
+    }
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: [
+          { // 配置 babel
+            loader: 'babel-loader',
+            options: {
+              // 设置预定义的环境
+              presets: [
+                ['@babel/preset-env', {
+                  // 要兼容的目标浏览器
+                  targets: { 'chrome': '88' },
+                  'corejs': '3', // 指定 corejs 的版本
+                  'useBuiltIns': 'usage', // 按需加载
+                }]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
         exclude: /node_modules/
       }
     ]
