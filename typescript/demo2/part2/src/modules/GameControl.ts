b26b85546daf523c6b6ee02049641acd5e9dbcfa
foxhsx <932163475@qq.com>
2022-03-27 22:32:58 +0800

    操作贪吃蛇移动

--- typescript/demo2/part2/src/modules/GameControl.ts
+++ typescript/demo2/part2/src/modules/GameControl.ts
@@ -6,7 +6,8 @@ class GameControl {
   snake: Snake;
   food: Food;
   scorePanel: ScorePanel;
-  direction: string = ''
+  direction: string = 'Right'
+  isLive = false
   constructor() {
     this.snake = new Snake()
     this.food = new Food()
@@ -18,6 +19,7 @@ class GameControl {
   init() {
     // 绑定键盘事件
     document.addEventListener('keydown', this.keydownHandler.bind(this))
+    this.run()
   }
 
   keydownHandler(event: KeyboardEvent) {
@@ -27,6 +29,47 @@ class GameControl {
     if (isTrue) {
       this.direction = event.key
     }
+  }
+
+  // 移动方法
+  run() {
+    /**
+     * 根据方向 this.direction 来使蛇的位置改变
+     * 上 top --
+     * 下 top ++
+     * 左 left --
+     * 右 right ++
+     */
+
+    // get current position
+    let X = this.snake.X
+    let Y = this.snake.Y
+
+    switch (this.direction) {
+      case "ArrowUp":
+      case "Up":
+        Y -= 10
+        break;
+      case "ArrowDown":
+      case "Down":
+        Y += 10;
+        break;
+      case "ArrowLeft":
+      case "Left":
+        X -= 10;
+        break;
+      case "ArrowRight":
+      case "Right":
+        X += 10
+        break;
+      default:
+        break;
+    }
+
+    this.snake.X = X
+    this.snake.Y = Y
+
+    this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30)
   }
 }
 
